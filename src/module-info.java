86c7103ff34bde4c3a38156a182d0947fc0e9227
Phi-Hoang-Ngo <vpgmarrero@outlook.com>
2022-11-20 14:08:36 -0600

    new

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author abstr
+ *
+ */
+module tutorial1 {
+}
---
+++ src/week5day3/application.java
@@ -0,0 +1,22 @@
+package week5day3;
+
+public class application {
+
+	public static void main(String[] args) {
+		int num ;
+		char letter;
+		boolean torf ;
+		long bigLong ;          
+		float someFloat ;  
+		double someDbl;     
+		short shortNum ;
+		byte byteA;
+		
+		System.out.println(num + letter + torf + bigLong + someFloat + someDbl + shortNum + byteA);
+		
+//new
+
+
+	}
+
+}
